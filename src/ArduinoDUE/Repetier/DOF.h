06cb704d33b1e04e266ca002b537dd8fe661e8fb
skapin <skapinthefourb@gmail.com>
2015-07-11 12:08:33 +0200

    add Super class

--- src/ArduinoDUE/Repetier/DOF.h
+++ src/ArduinoDUE/Repetier/DOF.h
@@ -6,7 +6,7 @@
 #include <inttypes.h>
 
 /**********************************************************************
- *  All credit to : https://github.com/sparkfun/LSM9DS0_Breakout 
+ *  All credit to : https://github.com/sparkfun/LSM9DS0_Breakout
  * ********************************************************************/
 // global constants for 9 DoF fusion and AHRS (Attitude and Heading Reference System)
 #define GyroMeasError PI * (40.0f / 180.0f)       // gyroscope measurement error in rads/s (shown as 3 deg/s)
@@ -16,8 +16,8 @@
 // However, with this value, the LSM9SD0 response time is about 10 seconds to a stable initial quaternion.
 // Subsequent changes also require a longish lag time to a stable output, not fast enough for a quadcopter or robot car!
 // By increasing beta (GyroMeasError) by about a factor of fifteen, the response time constant is reduced to ~2 sec
-// I haven't noticed any reduction in solution accuracy. This is essentially the I coefficient in a PID control sense; 
-// the bigger the feedback coefficient, the faster the solution converges, usually at the expense of accuracy. 
+// I haven't noticed any reduction in solution accuracy. This is essentially the I coefficient in a PID control sense;
+// the bigger the feedback coefficient, the faster the solution converges, usually at the expense of accuracy.
 // In any case, this is the free parameter in the Madgwick filtering and fusion scheme.
 #define beta sqrt(3.0f / 4.0f) * GyroMeasError   // compute beta
 #define zeta sqrt(3.0f / 4.0f) * GyroMeasDrift   // compute zeta, the other free parameter in the Madgwick scheme usually set to a small or zero value
@@ -25,45 +25,50 @@
 #define DOF_Ki 0.0f
 
 
+// Comment out this section if you're using SPI
+// SDO_XM and SDO_G are both grounded, so our addresses are:
+#define LSM9DS0_XM  0x1D // Would be 0x1E if SDO_XM is LOW
+#define LSM9DS0_G   0x6B // Would be 0x6A if SDO_G is LOW
+
 typedef struct Point
 {
-	float x;
-	float y;
-	float z;
+    float x;
+    float y;
+    float z;
 } Point;
 
-class DOF
+class DOF: public LSM9DS0
 {
   public:
-	
-	DOF();
-	~DOF(){};
-	
-	
-	Point a;
-	Point g;
-	Point m;
-	
-	/** Tait–Bryan angles **/
-	float current_pitch; // pitch = x
-	float current_roll; // roll = y
-	float current_yawl; // yawl = z
-	
-	float deltat = 0.0f;        // integration interval for both filter schemes
 
-	float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
-	float eInt[3] = {0.0f, 0.0f, 0.0f};       // vector to hold integral error for Mahony method
-	float temperature;
-	
-	void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
-	void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
-	
-	static bool angleEquals( float checked_value, float base, float marge )
-	{
-		return ( checked_value <= (base + marge ) && checked_value >= (base - marge ));
-	};
-	
-	Point getCurrentAngle();
+    DOF();
+    ~DOF(){};
+
+
+    Point a;
+    Point g;
+    Point m;
+
+    /** Tait–Bryan angles **/
+    float current_pitch; // pitch = x
+    float current_roll; // roll = y
+    float current_yawl; // yawl = z
+
+    float deltat = 0.0f;        // integration interval for both filter schemes
+
+    float q[4] = {1.0f, 0.0f, 0.0f, 0.0f};    // vector to hold quaternion
+    float eInt[3] = {0.0f, 0.0f, 0.0f};       // vector to hold integral error for Mahony method
+    float temperature;
+
+    void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
+    void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
+
+    static bool angleEquals( float checked_value, float base, float marge )
+    {
+        return ( checked_value <= (base + marge ) && checked_value >= (base - marge ));
+    };
+
+    Point getCurrentAngle();
 };
 
 
